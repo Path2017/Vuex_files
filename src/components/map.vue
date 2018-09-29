25f8f4eecf0320fb6946aecc6a0214aca66ee4f7
pathliu <path2017@126.com>
2018-09-29 10:18:51 +0800

    add 对象监听

--- src/components/map.vue
+++ src/components/map.vue
@@ -3,6 +3,31 @@
      add map vue
   </div>
 </template>
+<script>
+  export default {
+    data(){
+      return {
+        objVal: {
+          name: 'obj',
+          type: 'obj'
+        }
+      }
+    },
+    watch:{
+      objVal:{
+        handler(val,oldval){
+
+        },
+        deep: true,
+      }
+    },
+    methods:{
+      changeObj(){
+        this.objVal.name = 'newobj';
+      }
+    }
+  }
+</script>
 <style>
 
 </style>
