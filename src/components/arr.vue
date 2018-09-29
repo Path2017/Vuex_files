763d73abcfe059d2f56dc78d1ebea43324c07fc4
pathliu <path2017@126.com>
2018-09-29 10:33:59 +0800

    add 数组监听

---
+++ src/components/arr.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      arrList: [1,2,3,4,5]
+    };
+  },
+  watch: {
+    arrList: {
+      handler(val, oldval) {
+
+      },
+      deep: true
+    }
+  },
+  methods: {
+    changeArr() {
+      // this.arrList[0] = 10;
+      // 修改为：
+      this.$set(this.arrList, 0, 10);
+    }
+  }
+};
+</script>
